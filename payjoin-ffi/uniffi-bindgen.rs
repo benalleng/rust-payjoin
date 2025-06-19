36ea41e341f90e978d984a24659a800e35205ff0
user <benalleng@gmail.com>
2025-06-19 12:01:16 -0400

    Specify path based on the OS in uniffi_bindgen
    The uniffi_bindgen requires that the source binary be of a certain
    filepath type. This simply uses a std lib OS method to determine the
    correct filepath. We only really support linux and macos at the moment
    but it seems easy enough to add future support for Windows here.

--- payjoin-ffi/uniffi-bindgen.rs
+++ payjoin-ffi/uniffi-bindgen.rs
@@ -1,4 +1,14 @@
+use std::env;
+
 fn main() {
+    let mut source_code_path = "target/release/libpayjoin_ffi".to_owned();
+    if env::consts::OS == "linux" {
+        source_code_path.push_str(".so");
+    } else if env::consts::OS == "macos" {
+        source_code_path.push_str(".dylib");
+    } else {
+        source_code_path.push_str(".dll");
+    };
     #[cfg(feature = "uniffi")]
     uniffi::uniffi_bindgen_main();
     #[cfg(feature = "uniffi")]
@@ -6,7 +16,7 @@ fn main() {
         "src/payjoin_ffi.udl".into(),
         None,
         Some("dart/lib".into()),
-        "target/release/libpayjoin_ffi.dylib".into(),
+        source_code_path.as_str().into(),
         true,
     )
     .unwrap();
